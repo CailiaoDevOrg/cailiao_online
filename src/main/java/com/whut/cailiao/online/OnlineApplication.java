7a574bac517855403d1dea23812b9790d883f3ef
niuyang <xfny_hmily@hotmail.com>
2016-02-24 00:01:30 +0800

    init

---
+++ src/main/java/com/whut/cailiao/online/OnlineApplication.java
@@ -0,0 +1,24 @@
+package com.whut.cailiao.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by niuyang on 16/2/23.
+ */
+@SpringBootApplication
+@ImportResource("classpath:consumer.xml")
+public class OnlineApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(OnlineApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineApplication.class, args);
+    }
+}
---
+++ src/main/java/com/whut/cailiao/online/web/QuestionnaireController.java
@@ -0,0 +1,29 @@
+package com.whut.cailiao.online.web;
+
+import com.alibaba.fastjson.JSON;
+import com.whut.cailiao.api.model.questionnaire.Questionnaire;
+import com.whut.cailiao.api.service.questionnaire.QuestionnaireBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by niuyang on 16/2/23.
+ */
+@RestController
+public class QuestionnaireController {
+
+    @Autowired
+    private QuestionnaireBaseService questionnaireBaseService;
+
+    @RequestMapping("/{id}.html")
+    public String getItemById(@PathVariable int id) {
+        Questionnaire questionnaire = this.questionnaireBaseService.getQuestionnaireById(id);
+        if (questionnaire == null) {
+            return null;
+        }
+        return JSON.toJSONString(questionnaire);
+    }
+
+}
