217c42a6400b00ac6c0954a2a41795a290a16755
niuyang <xfny_hmily@hotmail.com>
2016-02-24 10:28:07 +0800

    增加测试controller，用于前段页面跳转测试
    增加测试controller，用于前段页面跳转测试

---
+++ src/main/java/com/whut/cailiao/online/web/test/TestController.java
@@ -0,0 +1,22 @@
+package com.whut.cailiao.online.web.test;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by niuyang on 16/2/24.
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/goto/{url}.html")
+    public String gotoUrl(@PathVariable String url) {
+        if (StringUtils.isNotBlank(url)) {
+            return url;
+        }
+        return "index";
+    }
+
+}
